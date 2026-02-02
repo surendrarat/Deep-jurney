bd45815281d3f6f52d30f624543085bcba0334f9
surendar rathor <surendrarathor2003@gmail.com>
2026-02-03 02:57:07 +0530

    same

--- Web development/java_script/closure.js
+++ Web development/java_script/closure.js
@@ -1,31 +1,30 @@
 // function who use the its outer function variable after its exicution end lexical environment
+// it is matter for data privacy 
 
 // function outer(){
 
 //     let count =0;
-
 //     function inner(){
 //         count++;
 //         console.log(count);
 //     }
 //     return inner;
 // }
-
 // const fn=outer();
 // fn();
 // fn();
 // fn();
 
 
-function test(){
-    let a=10;
-    return function (){
-            console.log(a);
-    };
-}
+// function test(){
+//     let a=10;
+//     return function (){
+//             console.log(a);
+//     };
+// }
 
-const a=test();
-const b=test();
+// const a=test();
+// const b=test();
 
-a()
-b()
+// a()
+// b()
