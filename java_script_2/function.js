6db075ad01289dbf8f618913213ba509e4d70e21
surendar rathor <surendrarathor2003@gmail.com>
2026-02-20 03:56:18 +0530

    more important topic on function

--- java_script_2/function.js
+++ java_script_2/function.js
@@ -22,3 +22,28 @@ const fire=(x,y,w)=>x+y+w;
 
 console.log(fire(3,54,34))
 console.log(water(32,5));
+
+// default parameter 
+function greet(name="suru"){
+    return 'hello ' +name;
+}
+
+greet();
+console.log(greet())
+
+// Rest parameter
+function run(...number){
+    return number.reduce((a,b)=>a+b,0);
+}
+
+let y=run(2,3,5,43,3);
+console.log(y);
+
+// callback when a functin pass form another function 
+function process(callback){
+    callback();
+}
+
+process(function(){
+    console.log("hello")
+})
