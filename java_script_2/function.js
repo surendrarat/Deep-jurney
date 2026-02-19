2d0e9a63a1166accbc435800816901850b7dd419
surendar rathor <surendrarathor2003@gmail.com>
2026-02-20 00:40:30 +0530

    this is the basic function code

---
+++ java_script_2/function.js
@@ -0,0 +1,12 @@
+// basic declaraction of the function
+
+function goa(name){
+return "hello " + name;
+}
+ 
+let suru='what is your name';
+goa(suru);
+
+console.log(goa(suru))
+// let a=suru;
+// console.log(goa());
