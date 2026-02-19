367a03d6a2c7577919348f8928a5cb6575cd9bcd
surendar rathor <surendrarathor2003@gmail.com>
2026-02-20 00:49:37 +0530

    arrow function

--- java_script_2/function.js
+++ java_script_2/function.js
@@ -1,12 +1,24 @@
 // basic declaraction of the function
 
-function goa(name){
-return "hello " + name;
-}
+// function goa(name){
+// return "hello " + name;
+// }
  
-let suru='what is your name';
-goa(suru);
+// let suru='what is your name';
+// let x=goa(suru);
 
-console.log(goa(suru))
+// console.log(x)
 // let a=suru;
 // console.log(goa());
+
+
+// arrow function
+const water=(a,b) =>{
+    return a+b;
+}
+
+// short form
+const fire=(x,y,w)=>x+y+w;
+
+console.log(fire(3,54,34))
+console.log(water(32,5));
