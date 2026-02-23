b7bde52d4b8f191c2486e3d0c0c3dcbcb118cb92
surendar rathor <surendrarathor2003@gmail.com>
2026-02-24 04:06:11 +0530

    additional closure example

--- java_script_2/closure_2.js
+++ java_script_2/closure_2.js
@@ -45,3 +45,13 @@ function match(name){
  cricket.increment();
 
 console.log(cricket.getscore());
+
+
+// -------------------------------------------------------
+
+for(var i=1; i<=3;i++)
+{
+    setTimeout( function()  {
+        console.log(i)
+    }, 1000);
+}
