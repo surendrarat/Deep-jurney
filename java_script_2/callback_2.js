d82c36587b0688280b76025c46ef76077e03e9be
surendar rathor <surendrarathor2003@gmail.com>
2026-02-24 06:49:59 +0530

    advance callback question

--- java_script_2/callback_2.js
+++ java_script_2/callback_2.js
@@ -2,17 +2,75 @@ setTimeout(() => {
     console.log(`exicute after delay`);
 }, 2000);
 
-setTimeout(function (){
+setTimeout(function () {
     console.log("exicute after 4 second delay");
-} , 4000)
+}, 4000)
 
 //---------------------------------------------------------
 
-function processUserInput(callback){
-    const name="suru";
+function processUserInput(callback) {
+    const name = "suru";
     callback(name);
 };
 
-processUserInput(function(name){
-    console.log("hello " +name)
+processUserInput(function (name) {
+    console.log("hello " + name)
 })
+
+
+//-----------------------------------------------
+function middleware1(req, next) {
+    console.log("Middleware 1");
+    req.user = "Suru";
+    next();
+}
+
+function middleware2(req, next) {
+    console.log("MIddleware 2");
+    req.auth = true;
+    next();
+}
+
+function finalHandler(req) {
+    console.log("Final Handler");
+    console.log(req);
+}
+function runmiddlewares(req, Middleware, finalHandler) {
+
+    let index = 0;
+
+
+    function next() {
+        if (index < Middleware.length) {
+            const current = Middleware[index];
+            index++;
+            current(req, next);
+
+        }
+        else {
+            finalHandler(req);
+        }
+    }
+    next();
+}
+
+
+runmiddlewares({}, [middleware1, middleware2], finalHandler);
+
+
+//-------------------------------------------------------------------
+
+function myMap(arr, callback) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result.push(callback(arr[i], i, arr));
+    }
+    return result;
+}
+
+const numbers = [1, 2, 3];
+const doubled = myMap(numbers, function (value) {
+    return value * 2;
+});
+console.log(doubled);
