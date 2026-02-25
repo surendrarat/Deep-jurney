9f69e2bf2009b4e862a7c754db749f409444da39
surendar rathor <surendrarathor2003@gmail.com>
2026-02-26 02:05:33 +0530

    this is show how microtask and macrotask are working on js engine

--- java_script_2/Asynchronous_AND_eventloop.js
+++ java_script_2/Asynchronous_AND_eventloop.js
@@ -5,14 +5,14 @@ setTimeout(() => {
 }, 0);
 
 console.log("End");
-
+console.log("\n\n")
 /*  
 javascript is single threaded
 one call stack , one thing executes at a time
 no parallel execution in main thread
 but 
 it can still handle 
-timers, API calls , Usr clicks , file reading
+timers, API calls , User clicks , file reading
 how 
 because of the Event loop Architechture
 
@@ -24,3 +24,41 @@ Callback queue
 microtask queue
 Event loop
 */
+
+// promise (microtasks ) vs macrotask(setTimeout)
+console.log("start");
+
+setTimeout(() => {
+    console.log("Timeout")
+}, 0);
+
+Promise.resolve().then(() => console.log("Promise"));;
+
+console.log("End");
+
+// microtask run before macrotasks
+// macrotasks- setInterval,setTimeout,DOM Event,setImmediate (Node) these are going into macrotasks queue or callback queue
+
+
+/*What is Microtask?
+
+Examples:
+Promise.then()
+Promise.catch()
+queueMicrotask()
+MutationObserver
+These go to:
+👉 Microtask Queue
+Microtasks have higher priority. */
+console.log("\n");
+
+console.log("A");
+
+setTimeout(() => console.log("B"), 0);
+
+Promise.resolve().then(() => {
+  console.log("C");
+  Promise.resolve().then(() => console.log("D"));
+});
+
+console.log("E");
