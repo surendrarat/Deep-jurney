79a78513a599c9ca182a7c11242f46c9775e365f
surendar rathor <surendrarathor2003@gmail.com>
2026-02-28 04:01:27 +0530

    promise example

---
+++ java_script_2/promise_2.js
@@ -0,0 +1,20 @@
+// microtask explosion 
+// every .then() create a microtask.
+// long chains
+
+for (let i=0; i<1000; i++){
+    Promise.resolve().then(() => {});
+}
+
+// create  10000 microtask this can starve macrotasks and delay redering this is called 'microtask starvation'
+
+// task
+
+Promise.resolve()
+.then(()=>{
+    console.log(1);
+    return Promise.resolve(2);
+})
+.then(console.log)
+
+console.log(3);
