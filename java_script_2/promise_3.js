9564439ae88e1e65272e87cf2dd2e0949aa28c12
surendar rathor <surendrarathor2003@gmail.com>
2026-03-02 02:31:02 +0530

    promise resolution process how return promise deal with different condition

--- java_script_2/promise_3.js
+++ java_script_2/promise_3.js
@@ -2,3 +2,68 @@
 return new Promise(resolve => {
   setTimeout(() => resolve(2), 0);
 });
+
+// promise resolution procedure
+/* 
+when you return something form .then()
+javascript runs an internal algorithm called 
+promise resolution procedure
+it decides how the next promise in the chain resolves
+this is the core of promise chaining
+the 4 critical cases when a .then() returns x 
+the engine checks 
+if x is a primitive
+return 5;
+then nextpromise resolves with 5
+internally 
+resolve (nextpromise,5)
+if x is a promise
+return promise.resolve(5)
+then the next promise waits for it
+it adopts its state.
+if inner promise fulfills -if inner promise fulfulls outer fulfills
+if inner rejects outer rejects.
+this is called 
+promise flattening
+
+if x is thenable (very Advanced concept )
+thenable =any oject with .then() method 
+return {
+then : function (resolve) {
+resolve(10);
+}
+};
+even though this is not a real promise
+javascript will treat it like one 
+it will call
+x.then(resolve , reject)
+this is called thenable assimilation
+this is extremely important in interoperability between libraries
+
+if x throws an error
+throw new Error ("Fail")
+next promise becomes rejected.
+*/
+
+Promise.resolve().then(() => {throw "Error A"})
+.then(()=>{
+    console.log("won't run");
+})
+.catch(err=>{
+    console.log("Caught:" ,err);
+});
+
+/* 
+promise chain behaves like synchronous try / catch:
+try{
+step1();
+step2();
+step3();
+
+}
+catch(e) {
+handleError();
+}
+but asynchonuous
+*/
+
