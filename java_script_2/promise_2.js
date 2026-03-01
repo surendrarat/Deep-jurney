aef0797ff287c60d8f217d56a042db4d4086ae21
surendar rathor <surendrarathor2003@gmail.com>
2026-03-01 06:19:49 +0530

    same no change

--- java_script_2/promise_2.js
+++ java_script_2/promise_2.js
@@ -2,8 +2,8 @@
 // every .then() create a microtask.
 // long chains
 
-for (let i=0; i<1000; i++){
-    Promise.resolve().then(() => {});
+for (let i = 0; i < 1000; i++) {
+    Promise.resolve().then(() => { });
 }
 
 // create  10000 microtask this can starve macrotasks and delay redering this is called 'microtask starvation'
@@ -11,10 +11,10 @@ for (let i=0; i<1000; i++){
 // task
 
 Promise.resolve()
-.then(()=>{
-    console.log(1);
-    return Promise.resolve(2);
-})
-.then(console.log)
+    .then(() => {
+        console.log(1);
+        return Promise.resolve(2);
+    })
+    .then(console.log)
 
 console.log(3);
